d3fd61eeb062c5756af61b922baa91b384564512
zhanghuaijiea <13311462893@189.cn>
2019-03-22 12:06:14 +0800

    banbenshengji

---
+++ src/abstrictclass.java
@@ -0,0 +1,4 @@
+
+public abstract class abstrictclass {
+    abstract void asd();
+}
---
+++ src/son.java
@@ -0,0 +1,12 @@
+
+public class son extends abstrictclass {
+    @Override
+    void asd() {
+        System.out.println("哈哈");
+    }
+
+    public static void main(String[] args) {
+        son s=new son();
+        s.asd();
+    }
+}
