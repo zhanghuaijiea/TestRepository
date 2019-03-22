5dcfab95fd17f94a2c3096713ea036394c11527f
zhanghuaijiea <13311462893@189.cn>
2019-03-22 12:06:14 +0800

    banbenshengji

--- src/test.java
+++ src/test.java
@@ -1,5 +1,11 @@
 
 public class test {
+    private String name;
+
+    public test(String name) {
+        this.name = name;
+    }
+
     public static void main(String[] args) {
         System.out.println("haha");
     }
